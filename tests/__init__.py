56df9cdcfccb1f26b5a7815f55ac478d0717420b
talentestors <talentestors@gmail.com>
2025-01-09 21:03:45 +0800

    ⚰️ Remove redundant code

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,9 +4,6 @@ import nonebot
 # 导入适配器
 
 nonebot.init()
-driver = nonebot.get_driver()
-env_config = driver.config
-env_run_evironment = str(env_config.environment).strip()
 
 # 加载插件
 nonebot.load_plugin("nonebot_plugin_oi_helper")
