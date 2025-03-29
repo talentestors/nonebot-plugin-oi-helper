3abfbe2fcae84b9d00c76b504f7d3b2ef3305cb1
talentestors <talentestors@gmail.com>
2025-03-29 16:45:12 +0800

    ♻️ refactor(config): Remove field_validator
    - update Field usage

--- nonebot_plugin_oi_helper/config.py
+++ nonebot_plugin_oi_helper/config.py
@@ -1,23 +1,14 @@
-from pydantic import BaseModel, field_validator
-from nonebot.log import logger
+from pydantic import BaseModel, Field
 
 
 class ScopedConfig(BaseModel):
-    user_key: str
-    username: str
-    req_url: str = "https://clist.by:443/api/v4/contest/?upcoming=true&filtered=true&order_by=start&format=json"
+    user_key: str = Field(..., min_length=0, description="API key for clist.by")
+    username: str = Field(..., min_length=0, description="Username for clist.by")
+    req_url: str = Field(
+        default="https://clist.by:443/api/v4/contest/?upcoming=true&filtered=true&order_by=start&format=json",
+        description="Request URL for clist.by",
+    )
 
 
 class Config(BaseModel):
     clist: ScopedConfig
-
-    @field_validator("clist")
-    @classmethod
-    def check_priority(cls, v: ScopedConfig) -> ScopedConfig:
-        if v.user_key is None or v.username is None:
-            logger.error("nonebot-plugin-oi-helper: api_key or username is None", v)
-            raise ValueError("nonebot-plugin-oi-helper: api_key or username is None")
-        if v.user_key == "" or v.username == "":
-            logger.error("nonebot-plugin-oi-helper: api_key or username is empty", v)
-            raise ValueError("nonebot-plugin-oi-helper: api_key or username is empty")
-        return v
