f2d9e28e71db28a898813498b56cafe34dbbaa75
talentestors <talentestors@gmail.com>
2025-04-17 15:04:56 +0800

    🔖 chore(release): v0.6.1

--- nonebot_plugin_oi_helper/__init__.py
+++ nonebot_plugin_oi_helper/__init__.py
@@ -33,7 +33,7 @@ __plugin_meta__ = PluginMetadata(
     supported_adapters=None,
     extra={
         "author": "talentestors",
-        "version": "0.6.0",
+        "version": "0.6.1",
         "tags": ["nonebot", "plugin", "OI", "ACM", "LeetCode"],
     },
 )
