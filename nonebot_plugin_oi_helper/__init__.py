7d904e2f42dd54e38c6d35d3eace89c454a1175e
talentestors <i@yuhiri.me>
2026-01-08 09:40:16 +0800

    🔖 publish: v0.7.7

--- nonebot_plugin_oi_helper/__init__.py
+++ nonebot_plugin_oi_helper/__init__.py
@@ -33,7 +33,7 @@ __plugin_meta__ = PluginMetadata(
     supported_adapters=None,
     extra={
         "author": "talentestors",
-        "version": "0.7.6",
+        "version": "0.7.7",
         "tags": ["nonebot", "plugin", "OI", "ACM", "LeetCode"],
     },
 )
