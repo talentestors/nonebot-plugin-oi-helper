c16524d72a2f37fe85abcd0b4eefbfb4fe114d2f
talentestors <i@yuhiri.me>
2026-01-20 13:53:40 +0800

    🔖 publish: v2.0.0

--- nonebot_plugin_oi_helper/__init__.py
+++ nonebot_plugin_oi_helper/__init__.py
@@ -2,16 +2,16 @@
 # Copyright (C) 2024  talentestors
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+# GNU Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU Affero General Public License
+# You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import nonebot
@@ -33,7 +33,7 @@ __plugin_meta__ = PluginMetadata(
     supported_adapters=None,
     extra={
         "author": "talentestors",
-        "version": "1.1.0",
+        "version": "2.0.0",
         "tags": ["nonebot", "plugin", "OI", "ACM", "LeetCode"],
     },
 )
